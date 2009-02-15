0ebbd2df456cdbd667eccfc5252889a817d65821
Jose Felix Gomez <jfgomez86@gmail.com>
2009-02-14 19:29:42 -0500

    Initial commit.

---
+++ number_to_code.rb
@@ -0,0 +1,78 @@
+class Integer
+  WORDS_HASH = {
+    "1" => "uno",
+    "2" => "dos",
+    "3" => "tres",
+    "4" => "cuatro",
+    "5" => "cinco",
+    "6" => "seis",
+    "7" => "siete",
+    "8" => "ocho",
+    "9" => "nueve",
+    "10" => "diez",
+    "11" => "once",
+    "12" => "doce",
+    "13" => "trece",
+    "14" => "catorce",
+    "15" => "quince",
+    "16" => "dieciseis",
+    "17" => "diecisiete",
+    "18" => "dieciocho",
+    "19" => "diecinueve",
+    "20" => "veinte",
+    "21" => "veintiuno",
+    "22" => "veintidos",
+    "23" => "veintitres",
+    "24" => "veinticuatro",
+    "25" => "veinticinco",
+    "26" => "veintiseis",
+    "27" => "veintisiete",
+    "28" => "veintiocho",
+    "29" => "veintinueve",
+    "30" => "treinta",
+    "40" => "cuarenta",
+    "50" => "cincuenta",
+    "60" => "sesenta",
+    "70" => "setenta",
+    "80" => "ochenta",
+    "90" => "noventa",
+    "100" => "cien",
+    "200" => "doscientos",
+    "300" => "trescientos",
+    "400" => "cuatrocientos",
+    "500" => "quinientos",
+    "600" => "seiscientos",
+    "700" => "setecientos",
+    "800" => "ochocientos",
+    "900" => "novecientos",
+  }
+
+  def to_words
+    digits = self.to_s.split("").count
+    if self < 31
+      return "" if self == 0
+      return WORDS_HASH[self.to_s]
+    elsif self < 100
+      return WORDS_HASH[self.to_s] if self % 10 == 0
+      return WORDS_HASH[(self/10*10).to_s] + " y " + (self % 10).to_words
+    elsif self < 1000
+      return WORDS_HASH[self.to_s] if self % 100 == 0
+      return "ciento " + (self % 100).to_words if self < 200
+      return (self/100*100).to_words + " " + (self % 100).to_words
+    elsif self < 1000000
+      return "mil" if self == 1000
+      return (self/1000).to_words + " mil" if self % 1000 == 0
+      return (self/1000).to_words + " mil " + (self % 1000).to_words 
+    else
+      return "Numero demasiado grande" if self > 999999999999
+      return "un millon" if self == 1000000
+      return "un millon " + (self % 1000000).to_words if self < 2000000
+      return (self/1000000).to_words + " millones" if self % 1000000 == 0
+      return (self/1000000).to_words + " millones " + (self % 1000000).to_words
+    end
+  end
+
+  def commify
+    self.to_s.reverse.gsub(/(\d\d\d)(?=\d)(?!\d*\.)/,'\1,').reverse
+  end
+end 
---
+++ test_number_to_code.rb
@@ -0,0 +1,79 @@
+require 'number_to_code'
+require 'test/unit'
+
+class TestNumberToWords < Test::Unit::TestCase
+  def test_number_to_words
+    assert_equal "uno", 1.to_words
+    assert_equal "dos", 2.to_words
+    assert_equal "tres", 3.to_words
+    assert_equal "cuatro", 4.to_words
+    assert_equal "cinco", 5.to_words
+    assert_equal "seis", 6.to_words
+    assert_equal "siete", 7.to_words
+    assert_equal "ocho", 8.to_words
+    assert_equal "nueve", 9.to_words
+
+    assert_equal "diez", 10.to_words
+    assert_equal "once", 11.to_words
+    assert_equal "doce", 12.to_words
+    assert_equal "trece", 13.to_words
+    assert_equal "catorce", 14.to_words
+    assert_equal "quince", 15.to_words
+    assert_equal "dieciseis", 16.to_words
+    assert_equal "diecisiete", 17.to_words
+    assert_equal "dieciocho", 18.to_words
+    assert_equal "diecinueve", 19.to_words
+    assert_equal "veinte", 20.to_words
+    assert_equal "veintiuno", 21.to_words
+    assert_equal "veintidos", 22.to_words
+    assert_equal "veintitres", 23.to_words
+    assert_equal "veinticuatro", 24.to_words
+    assert_equal "veinticinco", 25.to_words
+    assert_equal "veintiseis", 26.to_words
+    assert_equal "veintisiete", 27.to_words
+    assert_equal "veintiocho", 28.to_words
+    assert_equal "veintinueve", 29.to_words
+
+    assert_equal "treinta", 30.to_words
+    assert_equal "treinta y cuatro", 34.to_words
+    assert_equal "cuarenta y siete", 47.to_words
+    assert_equal "cincuenta y uno", 51.to_words
+    assert_equal "sesenta", 60.to_words
+    assert_equal "setenta y nueve", 79.to_words
+    assert_equal "ochenta y seis", 86.to_words
+    assert_equal "noventa y nueve", 99.to_words
+
+    assert_equal "cien", 100.to_words
+    assert_equal "doscientos", 200.to_words
+    assert_equal "trescientos", 300.to_words
+    assert_equal "cuatrocientos", 400.to_words
+    assert_equal "quinientos", 500.to_words
+
+    assert_equal "seiscientos sesenta y tres", 663.to_words
+    assert_equal "ochocientos setenta y seis", 876.to_words
+    assert_equal "quinientos sesenta y ocho", 568.to_words
+    assert_equal "quinientos dos", 502.to_words
+    assert_equal "trescientos cuarenta y seis", 346.to_words
+    assert_equal "ciento cuarenta y ocho", 148.to_words
+
+    assert_equal "seiscientos sesenta y tres mil", 663000.to_words
+    assert_equal "ochocientos setenta y seis mil", 876000.to_words
+    assert_equal "quinientos sesenta y ocho mil", 568000.to_words
+    assert_equal "quinientos dos mil", 502000.to_words
+    assert_equal "trescientos cuarenta y seis mil", 346000.to_words
+
+    assert_equal "seiscientos sesenta y tres mil setecientos cuarenta y seis",
+      663746.to_words
+    assert_equal "ocho mil setecientos sesenta y ocho", 8768.to_words
+    assert_equal "cincuenta y seis mil ochocientos treinta y seis", 56836.to_words
+    assert_equal "quinientos dos mil ciento veintitres", 502123.to_words
+    assert_equal "novecientos cuarenta y seis mil novecientos ochenta y siete", 946987.to_words
+
+    assert_equal "un millon seiscientos sesenta y tres mil setecientos cuarenta y seis",
+      1663746.to_words
+    assert_equal "cinco millones ocho mil setecientos sesenta y ocho", 5008768.to_words
+    assert_equal "diez millones cincuenta y seis mil ochocientos treinta y seis", 10056836.to_words
+    assert_equal "trescientos cuarenta millones quinientos dos mil ciento veintitres", 340502123.to_words
+    assert_equal "novecientos millones", 900000000.to_words
+  end
+end
